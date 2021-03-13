83ca48df884eb2ccaefa4883b44dc4d3756a0839
Student User <student@workstation.lab.example.com>
2021-03-13 03:33:07 -0500

    edited

--- sam/index.php
+++ sam/index.php
@@ -1,4 +1,8 @@
 <?php include("include/header.php"); ?>
-
+<html>
+<body>
+<center><h3>edited version of app through git cli.</h3></center>
+<body>
+</html>
 
 <?php include("include/footer.php"); ?>
